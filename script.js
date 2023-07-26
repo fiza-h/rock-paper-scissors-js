bd338328c9f25d0e051a2ad2c4ee6c32125a7c15
Fiza Hussain <hussainfiza5@gmail.com>
2023-07-26 17:29:43 +0500

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+//check which button is clicked
+//display corresponding image
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissors");
+let gameResult = document.getElementById("result");
+let chooseMove = document.getElementById("move");
+
+let computerSelection;
+let playerSelection;
+
+let level; //keeps track of level
+let p_score = 0 //player score
+let c_score = 0 //computer score
+
+rockBtn.addEventListener('click', function() {
+    console.log("Button clicked")
+    chooseMove.textContent = "";
+    displayImg.style.display = "flex";
+    displayImg.src = "Untitled design (2)/p_rock.png";
+    playerSelection = "rock";
+    computerSelection = getComputerChoice();
+    showComputerChoice();
+    computerDisplay.style.display = "flex";
+});
+
+paperBtn.addEventListener('click', function() {
+    console.log("Button clicked")
+    chooseMove.textContent = "";
+    displayImg.style.display = "flex";
+    displayImg.src = "Untitled design (2)/p_paper.png";
+    playerSelection = "paper";
+    computerSelection = getComputerChoice();
+    showComputerChoice();
+    computerDisplay.style.display = "flex";
+});
+
+scissorBtn.addEventListener('click', function() {
+    console.log("Button clicked")
+    chooseMove.textContent = "";
+    displayImg.style.display = "flex";
+    displayImg.src = "Untitled design (2)/p_scissor.png";
+    playerSelection = "scissor";
+    computerSelection = getComputerChoice();
+    showComputerChoice();
+    computerDisplay.style.display = "flex";
+});
+
+function showComputerChoice() {
+    if (computerSelection=="rock") {
+        computerDisplay.src = "Untitled design (2)/c_rock.png";
+    }
+    else if (computerSelection=="paper") {
+        computerDisplay.src = "Untitled design (2)/c_paper.png";
+    }
+    else if (computerSelection=="scissor") {
+        computerDisplay.src = "Untitled design (2)/c_scissor.png";
+    }
+
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    gameResult.textContent = result;
+}
+
 function getComputerChoice() {
     let n = Math.random();
     if (n <= 0.33) {
@@ -9,9 +73,9 @@ function getComputerChoice() {
     else return "scissor";
 }
 
-function playerSelection() {
-    return prompt("Rock, Paper or Scissors: ").toLowerCase();
-}
+// // function playerSelection() {
+// //     return prompt("Rock, Paper or Scissors: ").toLowerCase();
+// // }
 
 function playRound(playerSelection, computerSelection) {
     if ((computerSelection == "rock" && playerSelection == "scissor") || 
@@ -36,10 +100,6 @@ function capitalizeFirstLetter(inputString) {
 }
 
 function game() {
-    let level; //keeps track of level
-    let p_score = 0 //player score
-    let c_score = 0 //computer score
-
     for (level = 0; level <= 5; level++) {
 
         let computerSelection = getComputerChoice();
@@ -84,4 +144,4 @@ function scoreChecker(string) {
     }
 }
 
-game();
+// game();
